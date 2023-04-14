c2f453f96f301891595ba428d0193584a32154fb
Jorge <jorge@Air-de-Jorge.fibertel.com.ar>
2023-04-14 05:09:48 -0300

    d

--- components/ui/Navbar/index.tsx
+++ components/ui/Navbar/index.tsx
@@ -11,36 +11,20 @@ export const NavBar = () => {
       </Head>
 
       <Box>
-        <div data-aos="fade-down">
-          <nav style={{ backgroundColor: "white" }} className='glow-div'>
+
+          <nav style={{ backgroundColor: "white" }} >
             <Box
               display="flex"
               sx={{
                 justifContent: {
                   xs: "center",
-                  glow: {
-                    content: '""',
-                    position: "absolute",
-                    zIndex: -1,
-                    top: 0,
-                    left: 0,
-                    width: "100%",
-                    height: "100%",
-                    borderRadius: "50%",
-                    filter: "blur(20px)",
-                    opacity: 0,
-                    transition: "opacity 0.3s",
-                  },
-                  glowActive: {
-                    opacity: 1,
-                  },
                 },
               }}
             >
               <Image width={371} height={124} alt="" src="/logo450x150.png" />
             </Box>
           </nav>
-        </div>
+
       </Box>
     </>
   );
