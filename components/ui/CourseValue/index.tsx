521fb36641d44539b987edaab32241b4d9221709
Jorge <jorge@Air-de-Jorge.fibertel.com.ar>
2023-04-27 11:06:27 -0300

    d

--- components/ui/CourseValue/index.tsx
+++ components/ui/CourseValue/index.tsx
@@ -27,7 +27,7 @@ export const CourseValue: FC<Props> = ({ phrases, isMobile }) => {
                                             <Typography variant='body1' sx={{ fontSize: isMobile ? "12px" : '24px', color: '#380366', lineHeight: isMobile ? "20px" : '29px', fontWeight: isMobile ? "700" : '500', fontFamily: 'Montserrat' }}>{e.label}</Typography>
                                         </Box>
                                         <Box flex={1} />
-                                        <Box display={isMobile ? 'flex' : 'auto'} alignItems={isMobile ? 'center' : 'auto'}>
+                                        <Box display={isMobile ? 'flex' : 'flex'} alignItems={isMobile ? 'start' : 'center'} >
                                             <Typography variant='body1' sx={{ fontSize: isMobile ? "12px" : '24px', color: '#380366', lineHeight: '29px', fontWeight: isMobile ? "700" : '500', fontFamily: 'Montserrat' }}>{e.value}</Typography>
                                         </Box>
                                     </Box>
