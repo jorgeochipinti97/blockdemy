d7552d2393d58fbc5f1bf3e5f74058c2439487bc
Jorge <jorge@Air-de-Jorge.fibertel.com.ar>
2023-06-12 09:01:24 -0300

    d

--- pages/ofertapremium.tsx
+++ pages/ofertapremium.tsx
@@ -31,7 +31,16 @@ const OfertaPremiumPage = () => {
                                 <Typography variant='body1' sx={{ fontSize: '24px', fontWeight: '500' }}>Una última oportunidad para que podamos<br /> guiarte en este camino.</Typography>
                             </Box>
                             <Box sx={{ mx: 4, my: 1, color: '#380366', lineHeight: '29.26px' }}>
-                                <Typography sx={{ fontWeight: '500', fontSize: '24px' }} variant='body1'>Porque sabemos el <span style={{fontWeight:'800'}}/>IMPACTO POSITIVO</span> que está<br /> teniendo la comunidad, en cada alumno que<br /> llega a nosotros... <span style={{fontWeight:'800'}}/>buscando lo mismo que vos</span></Typography>
+                                <Typography>
+                                    Porque sabemos el                                     <span style={{ fontWeight: '800' }}>
+                                        IMPACTO POSITIVO</span> que está
+                                    <br /> teniendo la comunidad, en cada alumno que
+                                    <br /> llega a nosotros...{' '}
+                                    <span style={{ fontWeight: '800' }}>
+                                        buscando lo mismo que vos
+                                    </span>
+                                </Typography>
+
                             </Box>
                             <Box sx={{ mx: 4, my: 1, color: '#380366', lineHeight: '29.26px' }}>
                                 <Typography sx={{ fontWeight: '500', fontSize: '24px' }} variant='body1'>Encontrá en nuestra comunidad, la libertad<br /> financiera, una nueva fuente de ingresos, o solo<br /> una forma de alcanzar tus objetivos... </Typography>
@@ -70,14 +79,14 @@ const OfertaPremiumPage = () => {
                 <Box display='flex' justifyContent='center' sx={{ mt: 6, mb: 5 }}>
                     <Typography variant='body1' sx={{ fontSize: '13px', color: '#380366', fontWeight: '400', textAlign: 'center', lineHeight: '42.67px' }}>EL MONTO SE CARGARÁ A LA TARJETA YA INGRESADA</Typography>
                 </Box>
-                <Box display='flex' justifyContent='center' sx={{ mb:5}}>
+                <Box display='flex' justifyContent='center' sx={{ mb: 5 }}>
                     <Typography variant='body1' sx={{ fontSize: '19px', color: '#380366', fontWeight: '500', textAlign: 'center', lineHeight: '42.67px' }}>RECORDÁ QUE NO VOLVERÁS A VER ESTA OFERTA!</Typography>
                 </Box>
-                <Box display='flex' justifyContent='center' sx={{ mb:5}}>
+                <Box display='flex' justifyContent='center' sx={{ mb: 5 }}>
                     <Typography variant='body1' sx={{ fontSize: '19px', color: '#380366', fontWeight: '700', textAlign: 'center', lineHeight: '42.67px' }}>No, gracias</Typography>
                 </Box>
             </>
-        </LayoutComponent>
+        </LayoutComponent >
     )
 }
 
