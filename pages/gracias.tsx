fe95b61673da9dc1946897e076f8dab559cf1cb8
Jorge <jorge@Air-de-Jorge.fibertel.com.ar>
2023-06-13 06:52:16 -0300

    l2 git add .

--- pages/gracias.tsx
+++ pages/gracias.tsx
@@ -5,18 +5,21 @@ import React from 'react'
 import { useEffect, useRef } from 'react';
 import { Bounce, Elastic, gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import { useKeenSlider } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
 
 const Gracias = () => {
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
     gsap.registerPlugin(ScrollTrigger);
     const elementRef = useRef(null);
+    const [ref] = useKeenSlider<HTMLDivElement>()
 
     useEffect(() => {
         const element = elementRef.current;
 
         gsap.to(element, {
-            xPercent: isMobile ? '+=2' : '+=140',
+            xPercent: window.innerWidth < 500 ? '+=30' : '+=140',
             duration: 2,
             ease: Bounce.easeOut
         })
@@ -58,10 +61,53 @@ const Gracias = () => {
                         </Grid>
                     </Grid>
                     <Box display='flex' justifyContent={isMobile ? 'end' : 'center'}>
-                        <Box sx={{ width: isMobile ? '57px' : '118px', height: '30px', background: ' linear-gradient(272.21deg, #332482 9.12%, #2E47A0 377.67%);' }} />
+                        <Box sx={{ width: isMobile ? '57px' : '118px', height: '30px', background: isMobile ? '#380366' : ' linear-gradient(272.21deg, #332482 9.12%, #2E47A0 377.67%);' }} />
                     </Box>
+                    <Box ref={ref} className="keen-slider" sx={{ background: 'linear-gradient(92.77deg, #2E47A0 14.92%, #380366 90.66%);' }}>
 
-                    <Box sx={{ height: '806px', background: 'linear-gradient(92.77deg, #2E47A0 14.92%, #380366 90.66%);' }}>
+                        <Box className="keen-slider__slide" sx={{ height: '614px' }}>
+                            <Typography variant='body1' sx={{ fontSize: '25px', fontWeight: '800', color: 'white', textAlign: 'center', my: 4 }}>¿Cómo seguimos desde acá?</Typography>
+
+                            <Box sx={{ my: 8 }} display='flex' justifyContent='start' >
+                                <Box display='flex' flexDirection='column'>
+                                    <Box display='flex' justifyContent='start' sx={{ ml: 5 }}>
+                                        <Image src='/access.png' alt='' width={133} height={97} />
+                                    </Box>
+                                    <Box sx={{ textAlign: 'start', position: 'relative', bottom: 30, ml: 13 }}>
+                                        <Typography variant='body1' sx={{ fontSize: '18px', color: 'white', fontWeight: '800' }}>Tus accesos</Typography>
+                                    </Box>
+                                    <Box sx={{ textAlign: 'start', ml: 5 }}>
+                                        <Typography variant='body1' sx={{ fontSize: '18px', color: 'white', fontWeight: '500', lineHeight: '26.82px' }}><span style={{ fontWeight: '800' }}>Revisá la casilla de correo que<br /> ingresaste alhacer tu pago.</span> Ahí vas<br /> a recibir un correoelectrónico con el<br /> acceso a tu compra.</Typography>
+                                    </Box>
+                                    <Box sx={{ textAlign: 'start', ml: 5 }}>
+                                        <Typography variant='body1' sx={{ fontSize: '18px', color: 'white', fontWeight: '500', lineHeight: '26.82px', mt: 4 }}>Tené en cuenta que, si pagaste por<br /> más de un acceso (al Curso de<br /> Arbitraje Financiero, laMasterclass,<br /> nuestra Comunidad Premium, o los<br /> Encuentros EN VIVO), ¡vas a recibir<br /> un correo por cada uno!</Typography>
+                                    </Box>
+                                </Box>
+                            </Box></Box>
+                        <Box className="keen-slider__slide" sx={{ height: '614px', display:isMobile?  'auto':'none' }}>
+                            <Box sx={{ my: 8, }} display='flex' justifyContent='center' >
+                                <Box display='flex' flexDirection='column'>
+                                    <Box display='flex' justifyContent='start' >
+                                        <Image src='/contact.png' alt='' width={101} height={96} />
+                                    </Box>
+                                    <Box sx={{ textAlign: 'start', mt: 1 }}>
+                                        <Typography variant='body1' sx={{ fontSize: '24px', color: 'white', fontWeight: '800' }}>Contacto</Typography>
+                                    </Box>
+                                    <Box sx={{ textAlign: 'start', mt: 4 }}>
+                                        <Typography variant='body1' sx={{ fontSize: '18px', color: 'white', fontWeight: '700', lineHeight: '26.82px' }}>¿Tenés alguna duda y querés<br /> contactarnos?</Typography>
+                                    </Box>
+                                    <Box sx={{ textAlign: 'start', mt: 4 }}>
+                                        <Typography variant='body1' sx={{ fontSize: '18px', color: 'white', fontWeight: '500', lineHeight: '26.82px' }}>Podés enviarnos un e-mail a<br /> alumnos@blockacademy-argentina.com</Typography>
+                                    </Box>
+                                    <Box sx={{ textAlign: 'start', mt: 4 }}>
+                                        <Typography variant='body1' sx={{ fontSize: '18px', color: 'white', fontWeight: '500', lineHeight: '26.82px' }}>¡Te respondemos tan pronto como<br /> podamos!</Typography>
+                                    </Box>
+                                </Box>
+                            </Box>
+                        </Box>
+
+                    </Box>
+                    <Box sx={{ height: '806px', background: 'linear-gradient(92.77deg, #2E47A0 14.92%, #380366 90.66%);', display: isMobile ? 'none' : 'auto' }}>
                         <Grid container>
                             <Grid item md={6} lg={6} xl={6} >
                                 <Box sx={{ my: 8, ml: 5 }} display='flex' justifyContent='start' >
@@ -116,19 +162,19 @@ const Gracias = () => {
                             </Grid>
                         </Grid>
                     </Box>
-                    <Box display='flex' justifyContent='center'>
-                        <Box sx={{ width: '118px', height: '30px', background: ' linear-gradient(272.21deg, #332482 9.12%, #2E47A0 377.67%);' }} />
+                    <Box display='flex' justifyContent={isMobile ? 'start' : 'center'} >
+                        <Box sx={{ position: 'relative', bottom: 2, width: isMobile ? '57px' : '118px', height: '30px', background: isMobile ? '#2E47A0' : ' linear-gradient(272.21deg, #332482 9.12%, #2E47A0 377.67%);' }} />
                     </Box>
 
                     <Box sx={{ my: 5 }} display='flex' flexDirection='column'>
                         <Box>
-                            <Typography variant='body1' sx={{ fontWeight: '800', color: '#380366', textAlign: 'center', fontSize: '40px' }}>¡Felicidades!</Typography>
+                            <Typography variant='body1' sx={{ fontWeight: '800', color: '#380366', textAlign: isMobile ? 'start' : 'center', ml: isMobile ? 2 : 0, fontSize: '40px', }}>¡Felicidades!</Typography>
                         </Box>
                         <Box>
-                            <Typography variant='body1' sx={{ fontWeight: '800', color: '#380366', textAlign: 'center', fontSize: '40px' }}>Este es el primer escalon para llegar a donde queres estar</Typography>
+                            <Typography variant='body1' sx={{ fontWeight: isMobile ? '500' : '800', color: '#380366', textAlign: isMobile ? 'start' : 'center', ml: isMobile ? 2 : 0, fontSize: isMobile ? '25px' : '40px' }}>Este es el primer escalon <br style={{ display: isMobile ? 'auto' : 'none' }} /> para llegar a donde<br style={{ display: isMobile ? 'auto' : 'none' }} /> queres estar</Typography>
                         </Box>
                         <Box sx={{ my: 5 }}>
-                            <Typography variant='body1' sx={{ fontWeight: '500', color: '#380366', textAlign: 'center', fontSize: '29px' }}>¡Nos vemos pronto!</Typography>
+                            <Typography variant='body1' sx={{ fontWeight: '500', color: '#380366', textAlign: isMobile ? 'start' : 'center', ml: isMobile ? 2 : 0, fontSize: '29px' }}>¡Nos vemos pronto!</Typography>
                         </Box>
 
                     </Box>
--- pages/ventadeclases.tsx
+++ pages/ventadeclases.tsx
@@ -15,34 +15,37 @@ const Ventadeclases = () => {
                 <>
                     <Box sx={{ widht: '100%', backgroundColor: '#363333', py: .9 }}>
                         <Box display='flex' justifyContent='center' alignItems='center' flexDirection='column'>
-                            <Typography variant='body1' sx={{ color: 'white', fontSize: '22px', fontWeight: '700', textAlign: 'center', }}>¡FELICITACIONES! YA ADQUIRISTE EL CURSO DE ARBITRAJE FINANCIERO                        </Typography>
-                            <Typography variant='body1' sx={{ color: 'white', fontSize: '22px', fontWeight: '200', textAlign: 'center', }}>VERIFICÁ TU CASILLA DE CORREO, QUE VAS A RECIBIR TODA LA INFORMACIÓN PARA ACCEDER A TU CONTENIDO           </Typography>
+                            <Typography variant='body1' sx={{ color: 'white', fontSize: isMobile ? '17px' : '22px', fontWeight: '700', textAlign: 'center', }}>{isMobile ? '¡YA SOS PARTE DE LA COMUNIDAD BLOCK! NO TE PIERDAS LA EXPERIENCIA EN VIVO...' : 'FELICITACIONES! YA ADQUIRISTE EL CURSO DE ARBITRAJE FINANCIERO'}                        </Typography>
+                            <Typography variant='body1' sx={{ color: 'white', fontSize: isMobile ? '13px' : '22px', fontWeight: '200', textAlign: 'center', }}>{isMobile ? 'NO TE OLVIDES DE CHEQUEAR TU BANDEJA DE ENTRADA PARA RECIBIR EL ACCESO A TODOS TUS BENEFICIOS.' : 'VERIFICÁ TU CASILLA DE CORREO, QUE VAS A RECIBIR TODA LA INFORMACIÓN PARA ACCEDER A TU CONTENIDO'}          </Typography>
                         </Box>
                     </Box>
-                    <Box sx={{ widht: '100%', background: 'linear-gradient(286.04deg, #380366 38.08%, #2E47A0 83.7%)', py: 4 }}>
+                    <Box sx={{ widht: '100%', background: isMobile ? ' linear-gradient(180deg, #3A1D78 29.83%, #2E47A0 100%);':'linear-gradient(286.04deg, #380366 38.08%, #2E47A0 83.7%)', py: isMobile ? 2 : 4 }}>
                         <Grid container>
                             <Grid item md={6} lg={6} xl={6}>
-                                <Box display='flex' justifyContent='center' flexDirection='column' alignItems='center'>
+                                <Box display='flex' justifyContent='center' flexDirection='column' alignItems='center' sx={{ mx: 2 }}>
                                     <Box>
-                                        <Box sx={{ mt: 5 }}>
-                                            <Typography variant='body1' sx={{ fontWeight: '500', fontSize: '20px', color: 'white', }}>¡VIVÍ LA EXPERIENCIA BLOCK EN VIVO!</Typography>
+                                        <Box sx={{ mt: isMobile ? 2 : 5 }}>
+                                            <Typography variant='body1' sx={{ fontWeight: isMobile ? '700' : '500', fontSize: isMobile ? '16px' : '20px', color: 'white', }}>¡VIVÍ LA EXPERIENCIA BLOCK EN VIVO!</Typography>
                                         </Box>
-                                        <Typography variant='body1' sx={{ fontWeight: '700', fontSize: '57px', color: 'white', lineHeight: '69px' }}>Potenciá tus <br />redimientos<br /> al máximo</Typography>
+                                        <Typography variant='body1' sx={{ fontWeight: '700', fontSize: isMobile ? '40px' : '57px', color: 'white', lineHeight: isMobile ? '48.76px' : '69px', mt: isMobile ? 4 : 0 }}>Potenciá tus <br />redimientos<br /> al máximo</Typography>
                                         <Box alignSelf='start'>
-                                            <Divider sx={{ my: 4+1, py: .2, width: '178px', backgroundColor: 'white', borderRadius: '9px' }} />
+                                            <Divider sx={{ my: 4 + 1, py: .2, width: '178px', backgroundColor: 'white', borderRadius: '9px' }} />
                                         </Box>
                                         <Box >
-                                            <Typography variant='body1' sx={{ fontWeight: '500', fontSize: '22px', color: 'white', }}>Nada mejor para aprender en profundidad que <br /> nuestros <span style={{fontWeight:'800'}}>encuentros semanales EN VIVO: </span><br /> ¡aprendé estrategias exclusivas y obtené<br />información única <span style={{fontWeight:'800'}}>arbitrando en tiempo real</span><br /> con nuestros expertos! </Typography>
+                                            <Typography variant='body1' sx={{ fontWeight: '500', fontSize: isMobile ? '18px' : '22px', color: 'white', }}>Nada mejor para aprender en<br style={{ display: isMobile ? 'auto' : 'none' }} /> profundidad que <br style={{ display: isMobile ? 'none' : 'auto' }} /> nuestros <span style={{ fontWeight: '800' }}>encuentros<br style={{ display: isMobile ? 'auto' : 'none' }} /> semanales EN VIVO: </span><br style={{ display: isMobile ? 'none' : 'auto' }} /> ¡aprendé<br style={{ display: isMobile ? 'auto' : 'none' }} /> estrategias exclusivas y obtené<br />información única <span style={{ fontWeight: '800' }}>arbitrando en<br style={{ display: isMobile ? 'auto' : 'none' }} /> tiempo real</span><br style={{ display: isMobile ? 'none' : 'auto' }} /> con nuestros expertos! </Typography>
                                         </Box>
-                                        <Box sx={{ my: 3+1 }}>
-                                            <Typography variant='body1' sx={{ fontWeight: '500', fontSize: '22px', color: 'white', }}>Si llegaste hasta acá, estás a un paso de vivir la<br /> experiencia V.I.P. de Block Academy.</Typography>
+                                        <Box sx={{ my: 3 + 1 }}>
+                                            <Typography variant='body1' sx={{ fontWeight: '500', fontSize: isMobile ? '18px' : '22px', color: 'white', }}>Si llegaste hasta acá, estás a un paso de vivir la<br style={{ display: isMobile ? 'none' : 'auto' }} /> experiencia V.I.P. de Block Academy.</Typography>
                                         </Box>
-                                        <Box sx={{ mb: 3+1 }}>
-                                            <Typography variant='body1' sx={{ fontWeight: '500', fontSize: '22px', color: 'white', }}>Convertite en un miembro V.I.P. y <span style={{fontWeight:'800'}}>abrí la<br /> puerta hacia tu libertad financiera.</span></Typography>
+                                        <Box sx={{ mb: 3 + 1 }}>
+                                            <Typography variant='body1' sx={{ fontWeight: '500', fontSize: isMobile ? '18px' : '22px', color: 'white', }}>Convertite en un miembro V.I.P. y <span style={{ fontWeight: '800' }}>abrí la<br style={{ display: isMobile ? 'none' : 'auto' }} />puerta hacia tu libertad financiera.</span></Typography>
                                         </Box>
 
                                     </Box>
-                                    <Box display='flex' justifyContent='center'>
+                                    <Box sx={{ mt: 5, display: isMobile ? 'auto' : 'none' }}>
+                                        <Image src='/note.png' alt='image' width={355} height={197} />
+                                    </Box>
+                                    <Box display='flex' justifyContent={isMobile ? 'start' : 'center'} sx={{ display: isMobile ? 'none' : 'auto' }}>
                                         <Box sx={{ border: '3px dashed #FFFFFF', color: 'white', widht: 598, heigh: '48px', p: 1 }}>
                                             <Typography variant='body1' sx={{ fontSize: '20px', fontWeight: '700', textAlign: 'center', lineHeight: '24px' }}> ATENCIÓN: SER V.I.P. NO ES PARA INDECISOS<br /> Accedé solo si estás 100% comprometido con tu objetivo.</Typography>
                                         </Box>
@@ -50,29 +53,35 @@ const Ventadeclases = () => {
                                 </Box>
                             </Grid>
                             <Grid item md={6} lg={6} xl={6}>
-                                <Box display='flex' justifyContent='center' flexDirection='column' alignItems='center'>
-                                    <Box sx={{mt:5}}>
+                                <Box display='flex' justifyContent='center' sx={{ mx: isMobile ? 2 : 0 }} flexDirection='column' alignItems={isMobile ? 'start' : 'center'}>
+                                    <Box sx={{ mt: 5, display: isMobile ? 'none' : 'auto' }}>
                                         <Image src='/note.png' alt='image' width={547} height={303} />
                                     </Box>
-                                    <Box sx={{mt:1}}>
-                                        <Typography variant='body1' sx={{ fontSize: '24px', color: 'white', textAlign: 'center', lineHeight: '34px', fontWeight: '700' }}>
+                                    <Box sx={{ mt: 1 }}>
+                                        <Typography variant='body1' sx={{ fontSize: isMobile ? '17px' : '24px', color: 'white', textAlign: isMobile ? 'start' : 'center', lineHeight: isMobile ? '20.72px' : '34px', fontWeight: '700', my: isMobile ? 4 : 0 }}>
                                             Esta es la oportunidad que necesitás para<br /> llevar tus resultados al máximo nivel.
                                         </Typography>
                                     </Box>
-                                    <Box alignSelf='center'>
+                                    <Box alignSelf='center' sx={{ display: isMobile ? 'none' : 'auto' }}>
                                         <Divider sx={{ my: 4, py: .2, width: '390px', backgroundColor: 'white', borderRadius: '9px' }} />
                                     </Box>
-                                    <Box>
-                                        <Typography variant='body1' sx={{ fontSize: '24px', color: 'white', textAlign: 'center', lineHeight: '34px', fontWeight: '700' }}>
-                                            Convertite en un experto del<br /> arbitraje en criptomonedas...
+                                    <Box >
+                                        <Typography variant='body1' sx={{ fontSize: isMobile ? '15px' : '24px', color: 'white', textAlign: isMobile ? 'start' : 'center', lineHeight: isMobile ? '20.72px' : '34px', fontWeight: isMobile ? '500' : '700', }}>
+                                            Convertite en un experto del<br style={{ display: isMobile ? 'none' : 'auto' }} /> arbitraje en <br style={{ display: isMobile ? 'auto' : 'none' }} /> criptomonedas...
                                         </Typography>
                                     </Box>
-                                    <Box sx={{ mt: 4 }}>
-                                        <Typography variant='body1' sx={{ fontSize: '48px', color: '#F6E01E', fontWeight: '700' }}>
-                                            POR SOLO USD $49,99
+
+                                    <Box sx={{ mt: 4 }} alignSelf={isMobile ? 'center' : 'auto'}>
+                                        <Typography variant='body1' sx={{ fontSize: isMobile ? '42px' : '48px', color: '#F6E01E', fontWeight: '700', textAlign: isMobile ? 'center' : 'start' }}>
+                                            POR SOLO <br style={{ display: isMobile ? 'auto' : 'none' }} />  USD $49,99
                                         </Typography>
                                     </Box>
-                                    <Box sx={{ mt: 10 }}>
+                                    <Box display='flex' justifyContent={'center'} alignSelf='center' sx={{display:isMobile ? 'auto' :'none',mt:4}}>
+                                        <Box sx={{ border: '3px dashed #FFFFFF', color: 'white', widht: 598, heigh: '48px', p: 1 }}>
+                                            <Typography variant='body1' sx={{ fontSize: '13px', fontWeight: '700', textAlign: 'center', lineHeight: '19.5px' }}> ATENCIÓN: SER V.I.P. NO ES PARA INDECISOS<br /> Accedé solo si estás 100% comprometido <br/>con tu objetivo.</Typography>
+                                        </Box>
+                                    </Box>
+                                    <Box sx={{ mt: isMobile ? 4 : 10, }} alignSelf={isMobile ? 'center' : 'auto'}>
                                         <Button
                                             // onClick={handleClick}
                                             size="large"
@@ -85,6 +94,7 @@ const Ventadeclases = () => {
                                                 fontWeight: "bold",
                                                 fontSize: isMobile ? "20px" : "34px",
                                                 borderRadius: '23px',
+                                                width: isMobile ? '368px' : 'auto',
                                                 ":hover": {
                                                     bgcolor: "white",
                                                     boxShadow: '0px 0px 30px 10px rgba(255, 255, 255, 0.25)',
@@ -96,12 +106,14 @@ const Ventadeclases = () => {
                                             ACCEDER AHORA                                        </Button>
                                     </Box>
                                     <Box sx={{ mt: 1 }}>
-                                        <Typography variant='body1' sx={{ fontSize: '13px', color: 'white', fontWeight: '300' }}>
-                                            EL MONTO SE CARGARÁ A LA TARJETA YA INGRESADA
-                                        </Typography>
+                                        <Typography variant='body1' sx={{ fontSize: '13px', color: 'white', fontWeight: '300', textAlign:isMobile ? 'center':'start',mt:isMobile ? 3:0 }}>
+                                            {isMobile 
+                                          ?'NO NECESITÁS INGRESAR TUS DATOS DE NUEVO, SE CARGARÁ EL MONTO A LA MISMA TARJETA'  
+                                            :   'EL MONTO SE CARGARÁ A LA TARJETA YA INGRESADA'}
+'                                        </Typography>
                                     </Box>
-                                    <Box sx={{ mt: 2, mb: 4 }}>
-                                        <Typography variant='body1' sx={{ fontSize: '13px', color: 'white', fontWeight: '300' }}>
+                                    <Box sx={{ mt: 2, mb: 4 }} alignSelf={isMobile ? 'center' :'auto'}>
+                                        <Typography variant='body1' sx={{ fontSize: '13px', color: 'white', fontWeight: '300', textAlign:isMobile ? 'center':'start' }}>
                                             No gracias. No quiero las clases en vivo.
                                         </Typography>
                                     </Box>
@@ -111,12 +123,12 @@ const Ventadeclases = () => {
 
 
                     </Box>
-                    <Box display='flex' justifyContent='center'>
-                        <Box sx={{ background: 'linear-gradient(284.23deg, #351475 300.88%, #2E47A0 554.66%);', width: '118px', height: '30px' }} />
+                    <Box display='flex' justifyContent={isMobile ? 'end':'center'}>
+                        <Box sx={{ background: isMobile? '#2E47A0':'linear-gradient(284.23deg, #351475 300.88%, #2E47A0 554.66%);', width: isMobile ? '57px':'118px', height: '30px',position:'relative',bottom:2 }} />
                     </Box>
 
                     <Grid container sx={{ my: 4 }}>
-                        <Grid item md={4} lg={4} xl={4}>
+                        <Grid item md={4} lg={4} xl={4} sm={12} xs={12} sx={{my:isMobile ?3:0}}>
                             <Box display='flex' flexDirection='column' alignItems='space-between' justifyContent='space-between' sx={{ height: '180px', position: 'relative', bottom: 15 }}>
                                 <Box display='flex' justifyContent='center' alignItems='start'>
                                     <Image src='/camera.png' width={218} height={160} alt='' />
@@ -126,7 +138,7 @@ const Ventadeclases = () => {
                                 </Box>
                             </Box>
                         </Grid>
-                        <Grid item md={4} lg={4} xl={4}>
+                        <Grid item md={4} lg={4} xl={4} sm={12} xs={12} sx={{my:isMobile ?3:0}}>
                             <Box display='flex' flexDirection='column' alignItems='space-between' justifyContent='space-between' sx={{ height: '180px' }}>
                                 <Box display='flex' justifyContent='center' alignItems='start'>
                                     <Image src='/calendar.png' width={104} height={114} alt='' />
@@ -136,7 +148,7 @@ const Ventadeclases = () => {
                                 </Box>
                             </Box>
                         </Grid>
-                        <Grid item md={4} lg={4} xl={4}>
+                        <Grid item md={4} lg={4} xl={4} sm={12} xs={12} sx={{my:isMobile ?3:0}}>
                             <Box display='flex' flexDirection='column' alignItems='space-between' justifyContent='space-between' sx={{ height: '180px' }}>
                                 <Box display='flex' justifyContent='center' alignItems='start'>
                                     <Image src='/watch.png' width={117} height={126} alt='' />
@@ -147,7 +159,6 @@ const Ventadeclases = () => {
                             </Box>
                         </Grid>
                     </Grid>
-
                     <Box display='flex' justifyContent='center'>
                         <Button
                             // onClick={handleClick}
@@ -156,9 +167,10 @@ const Ventadeclases = () => {
                                 transition: 'all 0.3s ease-in-out',
                                 backgroundColor: "rgb(56, 3, 102)",
                                 boxShadow: "0px 0px 15px 5px rgba(84,108,233,.6)",
-                                fontSize: isMobile ? "20px" : "34px",
+                                fontSize: isMobile ? "18px" : "34px",
                                 fontWeight: '800', borderRadius: '29px',
-                                width: '1292px', mt: 4, height: '83px',
+                                lineHeight:isMobile ? '26.82px':'auto',
+                                width: isMobile ? '380px':'1292px', mt: 4, height: isMobile ? '95px':'83px',
                                 ":hover": {
                                     animation: "1s $titledButton",
                                     boxShadow: "0px 0px 15px 5px rgba(84,108,233,.9)",
@@ -178,11 +190,12 @@ const Ventadeclases = () => {
                             ¡Quiero acceder a las clases en vivo por USD $49.99!
                         </Button>
                     </Box>
+
                     <Box display='flex' justifyContent='center'>
                         <span style={{ fontSize: '11px', position: 'relative', fontWeight: 400, bottom: '20px', color: 'white' }}>El monto se cargará a la tarjeta ya ingresada</span>
                     </Box>
-                    <Box display='flex' justifyContent='center' sx={{my:4}}>
-                        <span style={{ fontSize: '15px',textAlign:'center',color:'#380366' }}>No gracias. No quiero las clases en vivo.</span>
+                    <Box display='flex' justifyContent='center' sx={{ my: 4 }}>
+                        <span style={{ fontSize: '15px', textAlign: 'center', color: '#380366' }}>No gracias. No quiero las clases en vivo.</span>
                     </Box>
                 </>
             </LayoutComponent>
