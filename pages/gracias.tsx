38108e8ba1487d5f9b3ff443a5d31c59c34c91a4
Jorge <jorge@Air-de-Jorge.fibertel.com.ar>
2023-06-13 05:22:23 -0300

    cart

--- pages/gracias.tsx
+++ pages/gracias.tsx
@@ -7,6 +7,8 @@ import { Bounce, Elastic, gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 
 const Gracias = () => {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
     gsap.registerPlugin(ScrollTrigger);
     const elementRef = useRef(null);
 
@@ -14,44 +16,49 @@ const Gracias = () => {
         const element = elementRef.current;
 
         gsap.to(element, {
-            xPercent: '+=140',
+            xPercent: isMobile ? '+=2' : '+=140',
             duration: 2,
             ease: Bounce.easeOut
         })
     }, []);
-    const theme = useTheme();
-    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
     return (
         <>
             <LayoutComponent title='BlockAcademy | Gracias' isMobile={isMobile}>
                 <>
                     <Box>
-                        <Typography variant='h1' sx={{ textAlign: 'center', fontSize: '64px', color: '#380366', fontWeight: '800' }}>¡Gracias por elegir Block Academy!</Typography>
+                        <Typography variant='h1' sx={{ textAlign: isMobile ? 'start' : 'center', ml: isMobile ? 2 : 0, fontSize: { xs: '34px', sm: '34px', md: '64px', lg: '64px', xl: '64px' }, color: '#380366', fontWeight: '800' }} >¡Gracias por elegir Block Academy!</Typography>
                     </Box>
                     <Box>
-                        <Typography variant='body1' sx={{ textAlign: 'center', fontSize: '33px', color: '#380366', fontWeight: '700' }}>Tu comunidad de arbitraje financiero en Argentina</Typography>
+                        <Typography variant='body1' sx={{ textAlign: 'center', fontSize: '33px', color: '#380366', fontWeight: '700', display: isMobile ? 'none' : 'auto' }}>Tu comunidad de arbitraje financiero en Argentina</Typography>
                     </Box>
-                    <Grid container sx={{ my: 4 }}>
-                        <Grid item md={6} lg={6} xl={6} >
-                            <Box display='flex' justifyContent='start' sx={{ height: '100%', position: 'absolute' }} >
-                                <Box ref={elementRef} sx={{ height: '200px' }}>
-                                    <Image src='/cart.png' width={212} height={197} alt='cart' />
+                    <Grid container sx={{ my: 4, overflow: 'hidden' }}>
+                        <Grid item md={6} lg={6} xl={6} sm={4} xs={4} >
+                            <Box display='flex' justifyContent={'start'} sx={{ height: '100%', position: isMobile ? 'auto' : 'absolute' }} >
+                                <Box sx={{ height: isMobile ? 'auto' : '200px' }} ref={elementRef}>
+                                    <Image src='/cart.png' width={isMobile ? 109 : 212} height={isMobile ? 101 : 197} alt='cart' />
                                 </Box>
                             </Box>
                         </Grid>
-                        <Grid item md={6} lg={6} xl={6} >
-                            <Box sx={{ height: '100%' }} display='flex' flexDirection='column' justifyContent='center'>
-                                <Box display='flex' justifyContent='start' sx={{ mt: 4 }} >
-                                    <Typography variant='body1' sx={{ textAlign: 'start', fontSize: '24px', lineheight: '29.26px', color: '#380366', fontWeight: '500' }}>Por favor, verificá tu casilla de correo electronico.<br /> ¡En los próximos minutos recibirás un correo con<br /> toda la información para comenzar cuanto antes!</Typography>
+                        <Grid item md={6} lg={6} xl={6} sm={8} xs={8} sx={{ display: isMobile ? 'auto' : 'none' }}>
+                            <Box display='flex' justifyContent='center' sx={{ height: '100%', position: isMobile ? 'auto' : 'absolute' }} alignItems='center'>
+                                <Box sx={{ height: isMobile ? 'auto' : '200px' }}>
+                                    <Typography variant='h1' sx={{ textAlign: 'start', fontSize: { xs: '20px', sm: '20px', md: '64px', lg: '64px', xl: '64px' }, color: '#380366', fontWeight: '800' }} >Tu comunidad de<br /> arbitraje financiero <br />en Argentina</Typography>
                                 </Box>
-                                <Box display='flex' justifyContent='start' sx={{ mt: 2 }}>
-                                    <Typography variant='body1' sx={{ textAlign: 'start', fontSize: '24px', lineheight: '29.26px', color: '#380366', fontWeight: '700' }}>No te olvides de revisar la carpeta de spam.</Typography>
+                            </Box>
+                        </Grid>
+                        <Grid item md={6} lg={6} xl={6} sm={12} xs={12}>
+                            <Box sx={{ height: '100%' }} display='flex' flexDirection='column' justifyContent='center'>
+                                <Box display='flex' justifyContent={isMobile ? 'center' : 'start'} sx={{ mt: 4 }} >
+                                    <Typography variant='body1' sx={{ width: isMobile ? '344px' : 'auto', textAlign: 'start', fontSize: isMobile ? '18px' : '24px', lineheight: '29.26px', color: '#380366', fontWeight: '500' }}>Por favor, verificá tu casilla de correo <br style={{ display: isMobile ? 'auto' : 'none' }} /> electronico.  <br style={{ display: isMobile ? 'none' : 'auto' }} /> ¡En los próximos minutos <br style={{ display: isMobile ? 'auto' : 'none' }} />recibirás un correo con<br style={{ display: isMobile ? 'none' : 'auto' }} /> toda la<br style={{ display: isMobile ? 'auto' : 'none' }} /> información para comenzar cuanto<br style={{ display: isMobile ? 'auto' : 'none' }} /> antes!</Typography>
+                                </Box>
+                                <Box display='flex' justifyContent={isMobile ? 'center' : 'start'} sx={{ mt: 2 }}>
+                                    <Typography variant='body1' sx={{ width: isMobile ? '344px' : 'auto', textAlign: 'start', fontSize: isMobile ? '18px' : '24px', lineheight: '29.26px', color: '#380366', fontWeight: '700' }}>No te olvides de revisar la carpeta de spam.</Typography>
                                 </Box>
                             </Box>
                         </Grid>
                     </Grid>
-                    <Box display='flex' justifyContent='center'>
-                        <Box sx={{ width: '118px', height: '30px', background: ' linear-gradient(272.21deg, #332482 9.12%, #2E47A0 377.67%);' }} />
+                    <Box display='flex' justifyContent={isMobile ? 'end' : 'center'}>
+                        <Box sx={{ width: isMobile ? '57px' : '118px', height: '30px', background: ' linear-gradient(272.21deg, #332482 9.12%, #2E47A0 377.67%);' }} />
                     </Box>
 
                     <Box sx={{ height: '806px', background: 'linear-gradient(92.77deg, #2E47A0 14.92%, #380366 90.66%);' }}>
@@ -126,7 +133,7 @@ const Gracias = () => {
 
                     </Box>
                 </>
-            </LayoutComponent>
+            </LayoutComponent >
 
         </>
     )
